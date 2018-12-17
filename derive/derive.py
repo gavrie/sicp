9f150c084fea261d65dbafe9ca90c5678c7eb169
Gavrie Philipson <gavrie@redislabs.com>
2018-12-17 23:18:43 +0200

    Strict mypy safety

--- derive/derive.py
+++ derive/derive.py
@@ -1,6 +1,8 @@
+from typing import cast
 import ast
-import astunparse
 import logging
+
+import astunparse  # type: ignore
 
 """
 Based on SICP section 2.3.2 Example: Symbolic Differentiation.
@@ -11,49 +13,66 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-def is_num(v: ast.AST) -> bool:
+def is_num(v: ast.expr) -> bool:
     logger.debug(f"is_num: {ast.dump(v)}")
     return isinstance(v, ast.Num)
 
 
-def num_equals(v1: ast.AST, v2: ast.AST):
+def num_equals(v1: ast.expr, v2: ast.expr) -> bool:
     logger.debug(f"num_equals: {ast.dump(v1)} == {ast.dump(v2)} ?")
-    return is_num(v1) and is_num(v2) and v1.n == v2.n
+
+    if not is_num(v1) or not is_num(v2):
+        return False
+
+    v1 = cast(ast.Num, v1)
+    v2 = cast(ast.Num, v2)
+
+    return v1.n == v2.n
 
 
-def is_var(v: ast.AST):
+def is_var(v: ast.expr) -> bool:
     logger.debug(f"is_var: {ast.dump(v)}")
     return isinstance(v, ast.Name)
 
 
-def var_equals(v1: ast.AST, v2: ast.AST):
+def var_equals(v1: ast.expr, v2: ast.expr) -> bool:
     logger.debug(f"var_equals: {ast.dump(v1)}, {ast.dump(v2)}")
-    return is_var(v1) and is_var(v2) and v1.id == v2.id
+
+    if not is_var(v1) or not is_var(v2):
+        return False
+
+    v1 = cast(ast.Name, v1)
+    v2 = cast(ast.Name, v2)
+
+    return v1.id == v2.id
 
 
-def is_sum(v: ast.AST) -> bool:
+def is_sum(v: ast.expr) -> bool:
     return (isinstance(v, ast.BinOp) and
             isinstance(v.op, ast.Add))
 
 
-def is_prod(v: ast.AST) -> bool:
+def is_prod(v: ast.expr) -> bool:
     return (isinstance(v, ast.BinOp) and
             isinstance(v.op, ast.Mult))
 
 
-def is_pow_num(v: ast.AST) -> bool:
+def is_pow_num(v: ast.expr) -> bool:
     return (isinstance(v, ast.BinOp) and
             isinstance(v.op, ast.Pow) and
             isinstance(v.right, ast.Num))
 
 
-def make_sum(a1: ast.AST, a2: ast.AST) -> ast.AST:
+def make_sum(a1: ast.expr, a2: ast.expr) -> ast.expr:
     if num_equals(a1, ast.Num(0)):
         return a2
     if num_equals(a2, ast.Num(0)):
         return a1
 
     if is_num(a1) and is_num(a2):
+        a1 = cast(ast.Num, a1)
+        a2 = cast(ast.Num, a2)
+
         return ast.Num(a1.n + a2.n)
 
     if a1 == a2:
@@ -62,13 +81,16 @@ def make_sum(a1: ast.AST, a2: ast.AST) -> ast.AST:
     return ast.BinOp(op=ast.Add(), left=a1, right=a2)
 
 
-def make_prod(m1: ast.AST, m2: ast.AST) -> ast.AST:
+def make_prod(m1: ast.expr, m2: ast.expr) -> ast.expr:
     if num_equals(m1, ast.Num(1)):
         return m2
     if num_equals(m2, ast.Num(1)):
         return m1
 
     if is_num(m1) and is_num(m2):
+        m1 = cast(ast.Num, m1)
+        m2 = cast(ast.Num, m2)
+
         return ast.Num(m1.n * m2.n)
 
     if num_equals(m1, ast.Num(0)) or num_equals(m2, ast.Num(0)):
@@ -77,7 +99,7 @@ def make_prod(m1: ast.AST, m2: ast.AST) -> ast.AST:
     return ast.BinOp(op=ast.Mult(), left=m1, right=m2)
 
 
-def make_pow(base: ast.AST, exp: ast.AST) -> ast.AST:
+def make_pow(base: ast.expr, exp: ast.expr) -> ast.expr:
     logger.info(f"make_pow: {ast.dump(base)} ** {ast.dump(exp)}")
 
     if num_equals(exp, ast.Num(0)):
@@ -89,7 +111,7 @@ def make_pow(base: ast.AST, exp: ast.AST) -> ast.AST:
     return ast.BinOp(op=ast.Pow(), left=base, right=exp)
 
 
-def _derive(e: ast.AST, var: ast.AST) -> ast.AST:
+def _derive(e: ast.expr, var: ast.expr) -> ast.expr:
     logger.debug(f"deriv: {ast.dump(e)}, {ast.dump(var)}")
 
     if is_num(e):
@@ -99,19 +121,22 @@ def _derive(e: ast.AST, var: ast.AST) -> ast.AST:
         return ast.Num(1) if var_equals(e, var) else ast.Num(0)
 
     if is_sum(e):
-        u, v = e.left, e.right
+        u = cast(ast.BinOp, e).left
+        v = cast(ast.BinOp, e).right
         return make_sum(
             _derive(u, var),
             _derive(v, var))
 
     if is_prod(e):
-        u, v = e.left, e.right
+        u = cast(ast.BinOp, e).left
+        v = cast(ast.BinOp, e).right
         return make_sum(
             make_prod(u, _derive(v, var)),
             make_prod(v, _derive(u, var)))
 
     if is_pow_num(e):
-        u, n = e.left, e.right
+        u = cast(ast.BinOp, e).left
+        n = cast(ast.Num, cast(ast.BinOp, e).right)
         return make_prod(
             make_prod(n, make_pow(u, ast.Num(n.n - 1))),
             _derive(u, var)
@@ -122,12 +147,12 @@ def _derive(e: ast.AST, var: ast.AST) -> ast.AST:
 
 ##########
 
-def _parse(s: str) -> ast.AST:
+def _parse(s: str) -> ast.expr:
     """
     Parse a string into a Python AST.
     """
     body = ast.parse(s).body
-    expr: ast.Expr = body[0]
+    expr = body[0]
 
     if not isinstance(expr, ast.Expr):
         raise Exception("Expected an expression")
@@ -139,4 +164,4 @@ def _parse(s: str) -> ast.AST:
 
 def derive(expr: str, var: str) -> str:
     result = _derive(_parse(expr), _parse(var))
-    return astunparse.unparse(result).strip()
+    return cast(str, astunparse.unparse(result).strip())
--- derive/test_derive.py
+++ derive/test_derive.py
@@ -1,23 +1,23 @@
 from .derive import derive
 
 
-def test_derive_basic():
+def test_derive_basic() -> None:
     assert derive("x+3", "x") == "1"
     assert derive("x*y", "x") == "y"
 
 
-def test_derive_poly_mult():
+def test_derive_poly_mult() -> None:
     assert derive("a*x*x + b*x + c", "x") == "(((a * x) + (x * a)) + b)"  # -> 2*a*x + b
 
 
-def test_derive_poly_2():
+def test_derive_poly_2() -> None:
     assert derive("a*x**2 + b*x + c", "x") == "((a * (2 * x)) + b)"  # -> 2*a*x + b
 
 
-def test_derive_poly_3():
+def test_derive_poly_3() -> None:
     assert derive("a*x**3 + b*x**2 + c*x + d", "x") == \
            "(((a * (3 * (x ** 2))) + (b * (2 * x))) + c)"
 
 
-def test_derive_poly_4():
+def test_derive_poly_4() -> None:
     assert derive("x**0.5", "x") == "(0.5 * (x ** -0.5))"
